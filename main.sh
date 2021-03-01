3e69e2a5dd798034c317406bb8aca55461f420cf
atasoya <45434939+atasoya@users.noreply.github.com>
2021-03-01 10:21:09 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+sudo apt install default-jdk
+sudo apt install screen
+mkdir minecraft
+cd minecraft
+wget https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
+mv server.jar minecraft_server.1.16.5.jar
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.16.5.jar nogui
+while read a; do
+    echo ${a//false/true}
+done < eula.txt > eula.txt.t
+mv eula.txt{.t,}
+screen -S "Minecraft server"
